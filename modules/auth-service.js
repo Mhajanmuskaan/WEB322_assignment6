ac99cf3b8ae77be42a0191f381bac1b256b8679e
Muskaan Mahajan <mhajanmuskaan1@gmail.com>
2025-04-06 23:20:31 -0400

    Initial commit for Assignment 6

---
+++ modules/auth-service.js
@@ -0,0 +1,111 @@
+const mongoose = require("mongoose");
+require('dotenv').config(); // Step 5
+
+const Schema = mongoose.Schema; // Step 4
+
+let User; // will be set in initialize()
+
+// Step 6: Create the userSchema
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true
+  },
+  password: String,
+  email: String,
+  loginHistory: [{
+    dateTime: Date,
+    userAgent: String
+  }]
+});
+
+function initialize() {
+    return new Promise(function (resolve, reject) {
+      let db = mongoose.createConnection(process.env.MONGODB);
+  
+      db.on('error', (err) => {
+        reject(err);
+      });
+  
+      db.once('open', () => {
+        User = db.model("users", userSchema);
+        resolve();
+      });
+    });
+  }
+
+  function registerUser(userData) {
+    return new Promise((resolve, reject) => {
+      if (userData.password !== userData.password2) {
+        reject("Passwords do not match");
+        return;
+      }
+  
+      let newUser = new User({
+        userName: userData.userName,
+        password: userData.password,
+        email: userData.email,
+        loginHistory: []
+      });
+  
+      newUser.save()
+        .then(() => resolve())
+        .catch(err => {
+          if (err.code === 11000) {
+            reject("User Name already taken");
+          } else {
+            reject("There was an error creating the user: " + err);
+          }
+        });
+    });
+  }
+
+  function checkUser(userData) {
+    return new Promise((resolve, reject) => {
+      User.find({ userName: userData.userName })
+        .then(users => {
+          if (users.length === 0) {
+            reject("Unable to find user: " + userData.userName);
+            return;
+          }
+  
+          if (users[0].password !== userData.password) {
+            reject("Incorrect Password for user: " + userData.userName);
+            return;
+          }
+  
+          // loginHistory tracking
+          if (users[0].loginHistory.length === 8) {
+            users[0].loginHistory.pop();
+          }
+  
+          users[0].loginHistory.unshift({
+            dateTime: (new Date()).toString(),
+            userAgent: userData.userAgent
+          });
+  
+          User.updateOne(
+            { userName: users[0].userName },
+            { $set: { loginHistory: users[0].loginHistory } }
+          ).then(() => {
+            resolve(users[0]);
+          }).catch(err => {
+            reject("There was an error verifying the user: " + err);
+          });
+  
+        })
+        .catch(() => {
+          reject("Unable to find user: " + userData.userName);
+        });
+    });
+  }
+
+  module.exports = {
+    initialize,
+    registerUser,
+    checkUser
+  };
+  
+  
+  
+  
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const projectService = require("./modules/projects"); // updated import name
+const authData = require("./modules/auth-service");
+const clientSessions = require("client-sessions");
 const path = require("path");
 
 const app = express();
@@ -16,6 +19,22 @@ app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//  Client Sessions Middleware
+app.use(clientSessions({
+  cookieName: "session",
+  secret: "aSuperSecretKeyYouShouldChange", // replace this with something secure later
+  duration: 2 * 60 * 1000, // 2 minutes
+  activeDuration: 1000 * 60 // extend by 1 minute if user is active
+}));
+
+//  Make session object available in all EJS views
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+
 
 // ✅ Home Route
 app.get("/", (req, res) => {
@@ -104,8 +123,37 @@ app.post("/post-request", (req, res) => {
     });
 });
 
-// ✅ GET route for Add Project form
-app.get("/solutions/addProject", (req, res) => {
+// ✅ Middleware to protect routes
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  next();
+}
+
+//  ✅ GET route for Add Project form
+// app.get("/solutions/addProject", (req, res) => {
+//   projectService.getAllSectors()
+//     .then(sectors => {
+//       res.render("addProject", { sectors });
+//     })
+//     .catch(err => {
+//       res.render("500", { message: `Unable to load form: ${err}` });
+//     });
+// });
+
+//  ✅ POST route to handle form submission
+// app.post("/solutions/addProject", (req, res) => {
+//   projectService.addProject(req.body)
+//     .then(() => {
+//       res.redirect("/solutions/projects");
+//     })
+//     .catch(err => {
+//       res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
+//     });
+// });
+
+app.get("/solutions/addProject", ensureLogin, (req, res) => {
   projectService.getAllSectors()
     .then(sectors => {
       res.render("addProject", { sectors });
@@ -115,8 +163,7 @@ app.get("/solutions/addProject", (req, res) => {
     });
 });
 
-// ✅ POST route to handle form submission
-app.post("/solutions/addProject", (req, res) => {
+app.post("/solutions/addProject", ensureLogin, (req, res) => {
   projectService.addProject(req.body)
     .then(() => {
       res.redirect("/solutions/projects");
@@ -127,8 +174,39 @@ app.post("/solutions/addProject", (req, res) => {
 });
 
 
+
 // ✅ GET: Load edit project form
-app.get("/solutions/editProject/:id", (req, res) => {
+// app.get("/solutions/editProject/:id", (req, res) => {
+//   const id = req.params.id;
+//   let sectorsData;
+
+//   projectService.getAllSectors()
+//     .then(sectors => {
+//       sectorsData = sectors;
+//       return projectService.getProjectsById(id);
+//     })
+//     .then(project => {
+//       res.render("editProject", { project, sectors: sectorsData });
+//     })
+//     .catch(err => {
+//       res.status(404).render("404", { message: err.message });
+//     });
+// });
+
+// ✅ POST: Handle project update form
+// app.post("/solutions/editProject", (req, res) => {
+//   const id = req.body.id;
+
+//   projectService.editProject(id, req.body)
+//     .then(() => {
+//       res.redirect("/solutions/projects");
+//     })
+//     .catch(err => {
+//       res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
+//     });
+// });
+
+app.get("/solutions/editProject/:id", ensureLogin, (req, res) => {
   const id = req.params.id;
   let sectorsData;
 
@@ -145,8 +223,7 @@ app.get("/solutions/editProject/:id", (req, res) => {
     });
 });
 
-// ✅ POST: Handle project update form
-app.post("/solutions/editProject", (req, res) => {
+app.post("/solutions/editProject", ensureLogin, (req, res) => {
   const id = req.body.id;
 
   projectService.editProject(id, req.body)
@@ -159,8 +236,21 @@ app.post("/solutions/editProject", (req, res) => {
 });
 
 
+
 // ✅ DELETE route to remove a project
-app.get("/solutions/deleteProject/:id", (req, res) => {
+// app.get("/solutions/deleteProject/:id", (req, res) => {
+//   const id = req.params.id;
+
+//   projectService.deleteProject(id)
+//     .then(() => {
+//       res.redirect("/solutions/projects");
+//     })
+//     .catch(err => {
+//       res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
+//     });
+// });
+
+app.get("/solutions/deleteProject/:id", ensureLogin, (req, res) => {
   const id = req.params.id;
 
   projectService.deleteProject(id)
@@ -171,6 +261,76 @@ app.get("/solutions/deleteProject/:id", (req, res) => {
       res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
     });
 });
+
+// ✅ GET /login
+app.get("/login", (req, res) => {
+  res.render("login", {
+    errorMessage: "",
+    userName: ""
+  });
+});
+
+// ✅ GET /register
+app.get("/register", (req, res) => {
+  res.render("register", {
+    errorMessage: "",
+    successMessage: "",
+    userName: ""
+  });
+});
+
+// ✅ POST /register
+app.post("/register", (req, res) => {
+  authData.registerUser(req.body)
+    .then(() => {
+      res.render("register", {
+        successMessage: "User created",
+        errorMessage: "",
+        userName: ""
+      });
+    })
+    .catch((err) => {
+      res.render("register", {
+        successMessage: "",
+        errorMessage: err,
+        userName: req.body.userName
+      });
+    });
+});
+
+// ✅ POST /login
+app.post("/login", (req, res) => {
+  req.body.userAgent = req.get('User-Agent');
+
+  authData.checkUser(req.body)
+    .then((user) => {
+      req.session.user = {
+        userName: user.userName,
+        email: user.email,
+        loginHistory: user.loginHistory
+      };
+      res.redirect("/solutions/projects");
+    })
+    .catch((err) => {
+      res.render("login", {
+        errorMessage: err,
+        userName: req.body.userName
+      });
+    });
+});
+
+// ✅ GET /logout
+app.get("/logout", (req, res) => {
+  req.session.reset();
+  res.redirect("/");
+});
+
+// ✅ GET /userHistory (Protected)
+app.get("/userHistory", ensureLogin, (req, res) => {
+  res.render("userHistory");
+});
+
+
 
 // ✅ 404 Error Page
 app.use((req, res) => {
@@ -183,7 +343,7 @@ app.use((req, res) => {
 });
 
 
-// // ✅ 404 Error Page
+// ✅ 404 Error Page
 // app.use((req, res) => {
 //     res.status(404).render("404", {
 //         message: "Page Not Found",
@@ -193,11 +353,27 @@ app.use((req, res) => {
 //     });
 // });
 
-// ✅ Start Server After DB Initialization
-projectService.initialize().then(() => {
+// ✅ Connect to MongoDB Atlas
+mongoose.connect("mongodb+srv://mhajanmuskaan1:6tKqUdLNq37hnYwV@cluster0.e1phf09.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+ useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log(" Connected to MongoDB Atlas");
+
+  // ✅ Start Server After DB Initialization
+  projectService.initialize()
+  .then(authData.initialize)
+  .then(() => {
     app.listen(HTTP_PORT, () => {
-        console.log(`Server is running on http://localhost:${HTTP_PORT}`);
+      console.log(`Server is running on http://localhost:${HTTP_PORT}`);
     });
-}).catch((err) => {
-    console.error("Failed to initialize project module:", err);
+  })
+  .catch((err) => {
+    console.error("Unable to start server:", err);
+  });
+
+})
+.catch((err) => {
+  console.error("MongoDB connection error:", err);
 });
